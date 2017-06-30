1c0b899bc29a367b62eedee3535b2b998cfdf00a
JachinWei <980615543@qq.com>
2017-07-01 00:30:15 +0800

    Update PhraseOMatic.java

--- demo1/PhraseOMatic.java
+++ demo1/PhraseOMatic.java
@@ -1,6 +1,5 @@
 package com.wyg.demo1;
 
-//专家术语学习机
 public class PhraseOMatic {
 	public static void main(String[] args) {
 		String[] wordListOne = {"24/7", "multiTier", "30,000 foot", "B-to-B", "win-win", "front-end", "web-based", "pervasive", "smart", "six-sigma", "critical-path", "dynamic"};
